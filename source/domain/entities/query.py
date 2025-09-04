47dd75077476cd7602ca23749f362c1f37be1869
vladimir <vovka1998@gmail.com>
2025-09-04 21:30:03 +0300

    updated domain entities

--- source/domain/entities/query.py
+++ source/domain/entities/query.py
@@ -3,3 +3,4 @@ from source.domain.entities.base import BaseEntity
 
 class QueryEntity(BaseEntity):
     url_vacancy: str
+    vacancy_id: str
--- source/domain/entities/response.py
+++ source/domain/entities/response.py
@@ -3,5 +3,7 @@ from source.domain.entities.base import BaseEntity
 
 class ResponseToVacancyEntity(BaseEntity):
     url_vacancy: str
-    text: str
+    vacancy_id: str
+    resume_id: str
+    message: str
     quality: bool | None = None
