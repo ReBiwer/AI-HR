521896f32efba84e082937000a5779c8ba3d8dee
vladimir <vovka1998@gmail.com>
2025-09-06 18:44:02 +0300

    added VacancyEntity

--- source/domain/entities/query.py
+++ source/domain/entities/query.py
@@ -1,6 +1,20 @@
+from typing import TypedDict
+
 from source.domain.entities.base import BaseEntity
 
 
 class QueryEntity(BaseEntity):
     url_vacancy: str
     vacancy_id: str
+
+
+class Experience(TypedDict):
+    id: str
+    name: str
+
+
+class VacancyEntity(BaseEntity):
+    name: str
+    experience: Experience
+    description: str
+    key_skills: set[str]
