c9d8989dd8c5bd7ee0be7d4919796fcc869dc487
vladimir <vovka1998@gmail.com>
2025-09-10 19:03:43 +0300

    updated OAuthHHUseCase

--- source/application/use_cases/auth_hh.py
+++ source/application/use_cases/auth_hh.py
@@ -1,11 +1,11 @@
 from source.application.services.hh_service import IHHService, AuthTokens
 
 
-class AuthHHUseCase:
+class OAuthHHUseCase:
 
     def __init__(self, hh_service: IHHService):
         self.hh_service = hh_service
 
-    async def __call__(self) -> AuthTokens:
-        tokens = await self.hh_service.auth()
+    async def __call__(self, code: str) -> AuthTokens:
+        tokens = await self.hh_service.auth(code)
         return tokens
