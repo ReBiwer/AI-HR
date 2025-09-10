0ac9651a3fe26be39e8e686d98b605885e8a8079
vladimir <vovka1998@gmail.com>
2025-09-10 11:38:37 +0300

    added main file

---
+++ source/main.py
@@ -0,0 +1,31 @@
+import uvicorn
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from source.presentation.api.auth import router as auth_router
+from source.infrastructure.settings.app import app_settings
+from source.infrastructure.di import init_di_container
+
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await app.state.dishka_container.close()
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title="AI-HR",
+        description="Clean Architecture implementation for AI HR",
+        lifespan=lifespan,
+    )
+
+    app.include_router(auth_router)
+    init_di_container(app)
+
+    return app
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:create_app", host='0.0.0.0', port=app_settings.BACKEND_PORT, factory=True, reload=True)
