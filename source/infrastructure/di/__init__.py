4c9f5a50f7677f8180a6b00485b40118e570d818
vladimir <vovka1998@gmail.com>
2025-09-10 20:15:51 +0300

    added use cases to dishka container

--- source/infrastructure/di/__init__.py
+++ source/infrastructure/di/__init__.py
@@ -3,12 +3,14 @@ from dishka import make_async_container
 from dishka.integrations.fastapi import setup_dishka
 
 from source.infrastructure.di.providers import (
-    ServicesProviders
+    ServicesProviders,
+    UseCasesProviders,
 )
 
 
 def init_di_container(app: FastAPI) -> None:
     container = make_async_container(
         ServicesProviders(),
+        UseCasesProviders(),
     )
     setup_dishka(container, app)
--- source/infrastructure/di/providers.py
+++ source/infrastructure/di/providers.py
@@ -4,6 +4,8 @@ from source.infrastructure.services.hh_service import HHService
 from source.infrastructure.services.ai_service import AIService
 from source.application.services.hh_service import IHHService
 from source.application.services.ai_service import IAIService
+from source.application.use_cases.generate_response import GenerateResponseUseCase
+from source.application.use_cases.auth_hh import OAuthHHUseCase
 
 
 class ServicesProviders(Provider):
@@ -14,3 +16,17 @@ class ServicesProviders(Provider):
     @provide
     def get_ai_service(self) -> IAIService:
         return AIService()
+
+
+class UseCasesProviders(Provider):
+    @provide
+    def get_generate_response_use_case(
+        self,
+        hh_service: IHHService,
+        ai_service: IAIService,
+        ) -> GenerateResponseUseCase:
+        return GenerateResponseUseCase(hh_service, ai_service)
+
+    @provide
+    def get_oauth_hh_use_case(self, hh_service: IHHService) -> OAuthHHUseCase:
+        return OAuthHHUseCase(hh_service)
