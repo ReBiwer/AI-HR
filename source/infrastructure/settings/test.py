4fd9f4ad4c180c37fc1b463436861f5314e62e36
vladimir <vovka1998@gmail.com>
2025-09-15 21:28:26 +0300

    added test settings

---
+++ source/infrastructure/settings/test.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class TestAppSettings(BaseSettings):
+    BASE_DIR: Path = Path(__file__).resolve().parent.parent.parent.parent
+    # Настройки для работы с API hh.ru
+    HH_CLIENT_ID: str
+    HH_CLIENT_SECRET: str
+    HH_REDIRECT_URI: str
+    HH_FAKE_SUBJECT: int = 1
+    HH_LOGIN: str
+    HH_PASSWORD: str
+
+    OPENAI_MODEL: str
+    OPENROUTER_BASE_URL: str = "https://openrouter.ai/api/v1"
+    OPENROUTER_API_KEY: str
+
+    model_config = SettingsConfigDict(env_file=f"/{BASE_DIR}/.env.test", extra="ignore")
+
+    # @property
+    # def db_url(self) -> str:
+    #     return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:5432/{self.DB_NAME}"
+    #
+    # @property
+    # def redis_url(self) -> str:
+    #     return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+
+test_app_settings = TestAppSettings()
