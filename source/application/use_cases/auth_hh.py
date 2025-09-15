7d24df143109e3c1513e4a5882b5bad059f9112e
vladimir <vovka1998@gmail.com>
2025-09-15 14:51:09 +0300

    updated OAuthHHUseCase

--- source/application/use_cases/auth_hh.py
+++ source/application/use_cases/auth_hh.py
@@ -1,11 +1,18 @@
+from typing import Mapping, Any
+
 from source.application.services.hh_service import IHHService, AuthTokens
+from source.application.services.state_manager import IStateManager, URL
+
 
 
 class OAuthHHUseCase:
 
-    def __init__(self, hh_service: IHHService):
+    def __init__(self, hh_service: IHHService, state_manager: IStateManager):
         self.hh_service = hh_service
+        self.state_manager = state_manager
 
-    async def __call__(self, code: str) -> AuthTokens:
+    async def __call__(self, code: str, state: str, request: Mapping[str, Any]) -> tuple[URL, AuthTokens]:
         tokens = await self.hh_service.auth(code)
-        return tokens
+        payload = await self.hh_service.get_me()
+        redirect_url = await self.state_manager.state_convert(state, payload.model_dump(), request)
+        return redirect_url, tokens
