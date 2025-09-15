63cd66010d366166538370cdaa9a366ab664f421
vladimir <vovka1998@gmail.com>
2025-09-15 15:48:02 +0300

    changed use case

--- source/application/use_cases/auth_hh.py
+++ source/application/use_cases/auth_hh.py
@@ -14,5 +14,6 @@ class OAuthHHUseCase:
     async def __call__(self, code: str, state: str, request: Mapping[str, Any]) -> tuple[URL, AuthTokens]:
         tokens = await self.hh_service.auth(code)
         payload = await self.hh_service.get_me()
-        redirect_url = await self.state_manager.state_convert(state, payload.model_dump(), request)
+        converted_payload = f"id={payload.id}, name={payload.name}"
+        redirect_url = await self.state_manager.state_convert(state, converted_payload, request)
         return redirect_url, tokens
