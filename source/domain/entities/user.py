d90754cb9fd12ba0bda9e9ba55a48345e8cc2fbe
vladimir <vovka1998@gmail.com>
2025-09-15 14:36:08 +0300

    added entity User

---
+++ source/domain/entities/user.py
@@ -0,0 +1,13 @@
+from pydantic import EmailStr
+
+from source.domain.entities.resume import ResumeEntity
+from source.domain.entities.base import BaseEntity
+
+
+class UserEntity(BaseEntity):
+    name: str
+    mid_name: str
+    last_name: str
+    phone: str
+    email: EmailStr | None = None
+    resume: ResumeEntity
