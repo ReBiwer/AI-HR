3bcbd84e52dfcc72c9f6fb3206f2e23c0435285a
vladimir <vovka1998@gmail.com>
2025-09-15 15:16:07 +0300

    changed field mid_name

--- source/domain/entities/user.py
+++ source/domain/entities/user.py
@@ -6,7 +6,7 @@ from source.domain.entities.base import BaseEntity
 
 class UserEntity(BaseEntity):
     name: str
-    mid_name: str
+    mid_name: str | None = None
     last_name: str
     phone: str
     email: EmailStr | None = None
