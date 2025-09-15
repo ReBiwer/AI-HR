b3487569d16f90b73e76396300d24e8984fba764
vladimir <vovka1998@gmail.com>
2025-09-15 14:41:04 +0300

    change typing to payload and change implementation to method state_convert

--- source/infrastructure/services/state_manager.py
+++ source/infrastructure/services/state_manager.py
@@ -1,18 +1,21 @@
+from typing import Mapping, Any
 from fastapi import Request
 from aiogram import Bot
 from aiogram.utils.deep_linking import create_deep_link
 
 from source.application.services.state_manager import IStateManager, URL
 from source.infrastructure.settings.app import app_settings
+from source.infrastructure.utils.jwt import encode_jwt
 
 
 class StateManager(IStateManager):
 
-    async def state_converter(self, state, payload: str, request: Request) -> URL:
+    async def state_convert(self, state, payload: Mapping[str, Any], request: Request) -> URL:
+        encoded_payload = encode_jwt(payload, app_settings.JWT_TOKEN)
         if state == "telegram":
             username_bot = (await Bot(app_settings.BOT_TOKEN).get_me()).username
-            bot_link = create_deep_link(username_bot, link_type="start", payload=payload)
+            bot_link = create_deep_link(username_bot, link_type="start", payload=encoded_payload)
             return bot_link
 
-        redirect_link = request.url_for(state, payload=payload)
+        redirect_link = request.url_for(state, payload=encoded_payload)
         return redirect_link.path
