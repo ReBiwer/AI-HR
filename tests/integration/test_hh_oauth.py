924d667406efe4ed5103a37566241e4029e57b8d
vladimir <vovka1998@gmail.com>
2025-09-16 13:23:08 +0300

    added test get tokens

--- tests/integration/test_hh_oauth.py
+++ tests/integration/test_hh_oauth.py
@@ -1,9 +1,11 @@
 import pytest
 from playwright.async_api import Page
 
+from source.application.services.hh_service import AuthTokens
 from source.infrastructure.settings.test import TestAppSettings
 
 
+@pytest.mark.skip
 @pytest.mark.usefixtures("run_test_server")
 async def test_oauth(page: Page, oauth_url: str, test_settings: TestAppSettings):
     await page.goto(oauth_url)
@@ -11,3 +13,8 @@ async def test_oauth(page: Page, oauth_url: str, test_settings: TestAppSettings)
     await page.get_by_role("button", name="Войти с паролем").click()
     await page.get_by_label("Пароль").fill(test_settings.HH_PASSWORD)
     await page.get_by_role("button", name="Войти").first.click()
+    await page.wait_for_timeout(1000)
+
+async def test_tokens(auth_tokens: AuthTokens):
+    assert auth_tokens
+    assert auth_tokens["access_token"] and auth_tokens["refresh_token"]
