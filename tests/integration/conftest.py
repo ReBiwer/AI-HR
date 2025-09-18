edf5eac64e389e08cb647dd598feafc87e2eacf5
vladimir <vovka1998@gmail.com>
2025-09-18 18:47:57 +0300

    updated fixtures

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -4,18 +4,22 @@ import time
 import pytest
 import socket
 import uvicorn
+import datetime
+from pathlib import Path
 from urllib.parse import urlparse
 from multiprocessing import Process
-from playwright.async_api import Page
+from playwright.async_api import Browser
 from hh_api.auth.token_manager import OAuthConfig
+from hh_api.auth.keyed_stores import TokenPair
 
 from source.main import create_app
 from source.infrastructure.services.hh_service import HHService
 from source.infrastructure.settings.test import TestAppSettings
 from source.application.services.hh_service import AuthTokens
+from source.domain.entities.vacancy import VacancyEntity
 
 
-@pytest.fixture()
+@pytest.fixture(scope="package")
 def hh_service(test_settings: TestAppSettings) -> HHService:
     service = HHService()
     service._hh_tm.config = OAuthConfig(
@@ -26,24 +30,20 @@ def hh_service(test_settings: TestAppSettings) -> HHService:
     return service
 
 
-@pytest.fixture()
-def mock_hh_service(hh_service: HHService, mocker):
-    mocker.patch(
-        "source.infrastructure.di.providers.HHService",
-        return_value=hh_service
-    )
-
-@pytest.fixture()
+@pytest.fixture(scope="package")
 def oauth_url(hh_service: HHService, test_settings: TestAppSettings) -> str:
     return hh_service.get_auth_url("test_tokens")
 
 
-@pytest.fixture(scope="session")
-def test_settings() -> TestAppSettings:
-    return TestAppSettings()
+@pytest.fixture(scope="package")
+def mock_hh_service(hh_service: HHService, session_mocker):
+    session_mocker.patch(
+        "source.infrastructure.di.providers.HHService",
+        return_value=hh_service
+    )
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="package")
 def run_test_server(test_settings: TestAppSettings, mock_hh_service):
     parsed = urlparse(test_settings.HH_REDIRECT_URI)
     host = parsed.hostname or "127.0.0.1"
@@ -76,7 +76,6 @@ def run_test_server(test_settings: TestAppSettings, mock_hh_service):
         raise RuntimeError(f"Server did not start on {host}:{port} within 15s")
 
     try:
-        print("сервер запущен")
         yield
     finally:
         if proc.is_alive():
@@ -84,9 +83,36 @@ def run_test_server(test_settings: TestAppSettings, mock_hh_service):
         proc.join(timeout=5.0)
 
 
-@pytest.fixture()
-async def auth_tokens(page: Page, oauth_url: str, test_settings: TestAppSettings, run_test_server):
-    await page.goto(oauth_url)
+@pytest.fixture(scope="package")
+def auth_storage_path(tmp_path_factory) -> Path:
+    return tmp_path_factory.mktemp("pw") / "auth.json"
+
+
+@pytest.fixture(scope="package", autouse=True)
+async def auth_tokens(
+        browser: Browser,
+        oauth_url: str,
+        test_settings: TestAppSettings,
+        hh_service: HHService,
+        run_test_server,
+        auth_storage_path
+):
+    """
+    Фикстура для oauth авторизации на hh.ru и сохранения токенов
+    :param browser: встроенная фикстура плагина playwright-pytest, представляет собой браузер
+    :param oauth_url: url куда нужно зайти для oauth авторизации
+    :param test_settings: фикстура, настройки тестов
+    :param hh_service: инстанс HHService, который у нас замокан в запущенном приложении
+    :param run_test_server: фикстура, запуск тестового сервиса для получения токенов
+    :param auth_storage_path: путь для сохранения storage_state (состояния контекста playwright)
+    :return:
+    """
+    # создаем контекст для хранения токенов в куках
+    context = await browser.new_context()
+    page = await context.new_page()
+
+    # процесс oauth авторизации
+    await page.goto(oauth_url, wait_until="domcontentloaded")
     await page.get_by_label("Электронная почта или телефон").fill(test_settings.HH_LOGIN)
     await page.get_by_role("button", name="Войти с паролем").click()
     await page.get_by_label("Пароль").fill(test_settings.HH_PASSWORD)
@@ -97,4 +123,39 @@ async def auth_tokens(page: Page, oauth_url: str, test_settings: TestAppSettings
     await page.wait_for_url(pattern)
     text = await page.text_content("pre, body")
     data = json.loads(text)
-    return AuthTokens(access_token=data["access_token"], refresh_token=data["refresh_token"])
+    tokens = AuthTokens(access_token=data["access_token"], refresh_token=data["refresh_token"])
+
+    # кладем полученные токены в куки браузера
+    await context.add_cookies([ # type: ignore
+        {"name": "access_token", "value": tokens["access_token"], "url": "http://localhost:8000"},
+        {"name": "refresh_token", "value": tokens["refresh_token"], "url": "http://localhost:8000"},
+    ])
+    # сохраняем контекст в файле и закрываем контекст
+    await context.storage_state(path=auth_storage_path)
+    await context.close()
+
+    # добавляем токены в инстанс HHService
+    token_pair = TokenPair(
+        access_token=tokens["access_token"],
+        refresh_token=tokens["refresh_token"],
+        expires_in=3600,
+        expires_at=datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=3600),
+    )
+    await hh_service.hh_client.tm.store.set_tokens(test_settings.HH_FAKE_SUBJECT, token_pair)
+
+    return tokens
+
+
+@pytest.fixture(scope="package")
+def browser_context_args(auth_storage_path):
+    return {"storage_state": auth_storage_path}
+
+
+@pytest.fixture(scope="package")
+async def test_vacancy(hh_service: HHService, test_settings: TestAppSettings) -> VacancyEntity:
+    vacancy, *_ = await hh_service.get_vacancies(
+        test_settings.HH_FAKE_SUBJECT,
+        per_page=10,
+        text="Python разработчик"
+    )
+    return vacancy
