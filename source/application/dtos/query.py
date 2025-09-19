bb81e84cd3834f4b3cb6d5d5ef15ee92b520e489
vladimir <vovka1998@gmail.com>
2025-09-19 18:32:37 +0300

    added default values

--- source/application/dtos/query.py
+++ source/application/dtos/query.py
@@ -1,6 +1,8 @@
+from pydantic import Field
+
 from source.application.dtos.base import BaseDTO
 
 
 class QueryCreateDTO(BaseDTO):
-    url_vacancy: str
-    resume_id: str
+    url_vacancy: str = Field(default="https://usinsk.hh.ru/vacancy/125537679")
+    resume_id: str = Field(default="6044a353ff0f1126620039ed1f42324e494b4c")
