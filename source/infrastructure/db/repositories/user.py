e5b52804d06f683b2ce609159dd338cf8b9ad30b
vladimir <vovka1998@gmail.com>
2025-09-25 15:06:37 +0300

    updated inheritance repositories

--- source/infrastructure/db/repositories/user.py
+++ source/infrastructure/db/repositories/user.py
@@ -1,8 +1,9 @@
 from source.infrastructure.db.repositories.base import SQLAlchemyRepository
 from source.infrastructure.db.models.user import UserModel
+from source.application.repositories.user import IUserRepository
 from source.domain.entities.user import UserEntity
 
 
-class UserRepository[ET: UserEntity, DBModel: UserModel](SQLAlchemyRepository):
+class UserRepository[ET: UserEntity, DBModel: UserModel](SQLAlchemyRepository, IUserRepository):
     model_class = UserModel
     entity_class = UserEntity
