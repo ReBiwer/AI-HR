cc86937db83d4970795b468f81b160031cae2431
vladimir <vovka1998@gmail.com>
2025-10-09 09:50:31 +0300

    added method for dump model to dict

--- source/infrastructure/db/models/base.py
+++ source/infrastructure/db/models/base.py
@@ -8,3 +8,24 @@ class BaseModel(DeclarativeBase):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     created_at: Mapped[DateTime] = mapped_column(DateTime, default=func.now())
+
+    def dump_dict(self) -> dict:
+        """
+        Метод для получения словаря с рекурсивным преобразованием сопутствующих сущностей
+        """
+        result_dict = {}
+        for attr, value in self.__dict__.items():
+            # если значение итерируемый объект, то проходим по каждому элементу
+            if isinstance(value, list):
+                converted_value = []
+                # проходимся по всем значениям последовательности
+                for val in value:
+                    # если это экземпляр нашего же класса, то вызываем рекурсию
+                    if isinstance(val, BaseModel):
+                        converted_value.append(val.dump_dict())
+                    else:
+                        converted_value.append(val)
+                result_dict[attr] = converted_value
+                continue
+            result_dict[attr] = value
+        return result_dict
