75a54adfa4dbf5500cd514d38784a620a3e3ea0b
vladimir <vovka1998@gmail.com>
2025-10-09 18:58:10 +0300

    create wsgi app

---
+++ source/presentation/wsgi.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from gunicorn.app.base import BaseApplication
+
+
+def get_app_options(
+    host: str,
+    port: int,
+    workers: int,
+    timeout: int,
+) -> dict:
+    return {
+        "accesslog": "-",
+        "errorlog": "-",
+        "loglevel": "info",
+        "bind": f"{host}:{port}",
+        "workers": workers,
+        "timeout": timeout,
+        "worker_class": "uvicorn.workers.UvicornWorker",
+    }
+
+
+class Application(BaseApplication):
+    def __init__(
+        self,
+        app: FastAPI,
+        options: dict | None = None,
+    ):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load(self):
+        return self.application
+
+    @property
+    def config_options(self) -> dict:
+        return {
+            k: v
+            for k, v in self.options.items()
+            if k in self.cfg.settings and v is not None
+        }
+
+    def load_config(self):
+        for key, value in self.config_options.items():
+            self.cfg.set(key.lower(), value)
