4fc65e2adcbffc3b72949996501919b520f5b0b8
vladimir <vovka1998@gmail.com>
2025-10-09 09:51:43 +0300

    updated method "update" base's SQLAlchemyRepository

--- source/infrastructure/db/repositories/base.py
+++ source/infrastructure/db/repositories/base.py
@@ -16,29 +16,37 @@ class SQLAlchemyRepository[ET: BaseEntity, DBModel: BaseModel](ISQLRepository[ET
     model_class: type[DBModel]
     entity_class: type[ET]
 
+    def _validate_entity_to_db_model(self, data: ET) -> DBModel:
+        raise NotImplementedError
+
     async def get(self, id_entity: int) -> ET | None:
         stmt = select(self.model_class).where(self.model_class.id == id_entity)
         result = await self.session.execute(stmt)
         model_instance = result.scalar_one_or_none()
         if model_instance:
-            return self.entity_class.model_validate(model_instance)
+            return self.entity_class.model_validate(model_instance.dump_dict())
         return None
 
     async def create(self, entity: ET) -> ET:
-        model_instance = self.model_class(**entity.model_dump())
+        model_instance = self._validate_entity_to_db_model(entity)
         self.session.add(model_instance)
         await self.session.flush()
-        return self.entity_class.model_validate(model_instance)
+        return self.entity_class.model_validate(model_instance.dump_dict())
 
     async def update(self, entity: ET) -> ET:
+        if entity.id is None:
+            msg = f"Невозможно обновить {self.entity_class.__name__} без идентификатора"
+            raise ValueError(msg)
+
         model_instance = await self.session.get(self.model_class, entity.id)
         update_data = entity.model_dump(exclude_unset=True)
 
         for key, value in update_data.items():
-            setattr(model_instance, key, value)
+            if not isinstance(key, BaseModel.__class__) and not isinstance(value, list):
+                setattr(model_instance, key, value)
 
         await self.session.flush()
-        return self.entity_class.model_validate(model_instance)
+        return self.entity_class.model_validate(model_instance.dump_dict())
 
     async def delete(self, id_entity: int) -> None:
         model_instance = await self.session.get(self.model_class, id_entity)
