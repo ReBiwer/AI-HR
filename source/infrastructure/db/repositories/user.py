020cd528a9a817d6d2a634dcc2487fb291b7cffb
vladimir <vovka1998@gmail.com>
2025-10-09 09:54:51 +0300

    added implementation method's _validate_entity_ro_db_model

--- source/infrastructure/db/repositories/user.py
+++ source/infrastructure/db/repositories/user.py
@@ -1,5 +1,6 @@
 from source.infrastructure.db.repositories.base import SQLAlchemyRepository
 from source.infrastructure.db.models.user import UserModel
+from source.infrastructure.db.models.resume import ResumeModel, JobExperienceModel
 from source.application.repositories.user import IUserRepository
 from source.domain.entities.user import UserEntity
 
@@ -9,3 +10,19 @@ class UserRepository[ET: UserEntity, DBModel: UserModel](
 ):
     model_class = UserModel
     entity_class = UserEntity
+
+    def _validate_entity_to_db_model(self, data: ET) -> DBModel:
+        resume_instances = [
+            ResumeModel(
+                job_experience=[
+                    JobExperienceModel(**job_exp.model_dump())
+                    for job_exp in resume.job_experience
+                ],
+                **resume.model_dump(exclude={"job_experience"}),
+            )
+            for resume in data.resumes
+        ]
+        model_instance = self.model_class(
+            **data.model_dump(exclude={"resumes"}), resumes=resume_instances
+        )
+        return model_instance
