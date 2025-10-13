215c65c9863d8dc5ea1cffaf649d2b60d04fdd7a
vladimir <vovka1998@gmail.com>
2025-10-13 14:49:25 +0300

    added implementation _check_exist_entity

--- source/infrastructure/db/repositories/user.py
+++ source/infrastructure/db/repositories/user.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from source.infrastructure.db.repositories.base import SQLAlchemyRepository
 from source.infrastructure.db.models.user import UserModel
 from source.infrastructure.db.models.resume import ResumeModel, JobExperienceModel
@@ -26,3 +27,14 @@ class UserRepository[ET: UserEntity, DBModel: UserModel](
             **data.model_dump(exclude={"resumes"}), resumes=resume_instances
         )
         return model_instance
+
+    async def _check_exist_entity(self, data: ET) -> DBModel | None:
+        entity_hh_id = getattr(data, "hh_id", None)
+        if entity_hh_id is None:
+            return None
+
+        exists_stmt = select(self.model_class).where(
+            self.model_class.hh_id == data.hh_id
+        )
+        result = await self.session.scalar(exists_stmt)
+        return result
