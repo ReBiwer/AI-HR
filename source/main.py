876f7c17a7f46006908283aaa1e4af41b8caaa44
vladimir <vovka1998@gmail.com>
2025-10-16 14:56:06 +0300

    added param for run main.py (telegram or web)

--- source/main.py
+++ source/main.py
@@ -6,6 +6,7 @@ from source.presentation.api.ai import router as ai_router
 from source.infrastructure.settings.app import app_settings
 from source.infrastructure.di import init_di_container
 from source.presentation.wsgi import Application, get_app_options
+from source.presentation.bot.create_bot import create_bot_app
 
 
 @asynccontextmanager
@@ -14,7 +15,7 @@ async def lifespan(app: FastAPI):
     await app.state.dishka_container.close()
 
 
-def create_app() -> FastAPI:
+def create_web_app() -> FastAPI:
     app = FastAPI(
         title="AI-HR",
         description="Clean Architecture implementation for AI HR",
@@ -29,13 +30,37 @@ def create_app() -> FastAPI:
 
 
 if __name__ == "__main__":
-    gunicorn_app = Application(
-        app=create_app(),
-        options=get_app_options(
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Какое приложение запускать")
+    parser.add_argument(
+        "--type-app",
+        type=str,
+        choices=("telegram", "web"),
+        default="telegram",
+        help="Какое приложение запустить",
+    )
+    args = parser.parse_args()
+
+    if args.type_app == "telegram":
+        app = create_bot_app()
+        options = get_app_options(
+            host=app_settings.BOT_APP_HOST,
+            port=app_settings.BOT_APP_PORT,
+            timeout=900,
+            workers=4,
+        )
+    else:
+        app = create_web_app()
+        options = get_app_options(
             host=app_settings.BACKEND_HOST,
             port=app_settings.BACKEND_PORT,
             timeout=900,
             workers=4,
-        ),
+        )
+
+    gunicorn_app = Application(
+        app=app,
+        options=options,
     )
     gunicorn_app.run()
