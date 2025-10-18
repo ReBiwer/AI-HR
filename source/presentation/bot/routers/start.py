27874b6eb18f640ece25f17bb0e69e507bb535da
vladimir <vovka1998@gmail.com>
2025-10-18 10:55:06 +0300

    updated start handler

--- source/presentation/bot/routers/start.py
+++ source/presentation/bot/routers/start.py
@@ -1,6 +1,8 @@
+import json
 from aiogram.types import Message
 from aiogram.dispatcher.router import Router
-from aiogram.filters.command import CommandStart
+from aiogram.filters.command import CommandStart, CommandObject
+from aiogram.utils.payload import decode_payload
 from dishka import FromDishka
 
 from source.application.services.hh_service import IHHService
@@ -9,17 +11,17 @@ from source.application.services.hh_service import IHHService
 router = Router()
 
 
-@router.message(CommandStart)
+@router.message(CommandStart())
 async def start(
     message: Message,
-    # command: CommandObject,
     hh_service: FromDishka[IHHService],
+    command: CommandObject = None,
 ):
-    # args = command.args
-    # if args:
-    #     payload = decode_payload(args)
-    #     print(payload)
-    #     return
+    args = command.args
+    if args:
+        payload = json.loads(decode_payload(args))
+        await message.answer(f"Приятно познакомиться {payload["name"]}")
+        return
     auth_url = hh_service.get_auth_url("telegram")
     await message.answer(
         f"Добрый день, перед началом работы нужно <a href='{auth_url}'>авторизоваться</a> в HH",
