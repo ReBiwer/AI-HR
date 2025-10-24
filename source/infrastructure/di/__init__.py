b0d33c717b9ce4c3c5e6f73cede089be56b10115
vladimir <vovka1998@gmail.com>
2025-10-24 10:58:12 +0300

    changed init di containers

--- source/infrastructure/di/__init__.py
+++ source/infrastructure/di/__init__.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from aiogram import Dispatcher
 from dishka import make_async_container, AsyncContainer
 from dishka.integrations.fastapi import setup_dishka as fastapi_setup
-from dishka.integrations.aiogram import setup_dishka as aiogram_setup
+from dishka.integrations.aiogram import setup_dishka as aiogram_setup, AiogramProvider
 
 from source.infrastructure.di.providers import (
     ServicesProviders,
@@ -17,9 +17,18 @@ def container_factory() -> AsyncContainer:
     )
 
 
+def bot_container_factory() -> AsyncContainer:
+    return make_async_container(
+        ServicesProviders(),
+        UseCasesProviders(),
+        RepositoriesProviders(),
+        AiogramProvider(),
+    )
+
+
 def init_di_container(app: FastAPI) -> None:
     fastapi_setup(container_factory(), app)
 
 
 def init_di_container_bot(dp: Dispatcher) -> None:
-    aiogram_setup(container_factory(), router=dp, auto_inject=True)
+    aiogram_setup(bot_container_factory(), router=dp, auto_inject=True)
