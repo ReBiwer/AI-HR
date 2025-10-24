c3cf364df26a04d0bc35ea947e930b1e3ccec543
vladimir <vovka1998@gmail.com>
2025-10-24 20:50:53 +0300

    updated method __str__

--- source/domain/entities/user.py
+++ source/domain/entities/user.py
@@ -13,3 +13,12 @@ class UserEntity(BaseEntity):
     email: EmailStr | None = None
     telegram_id: int | None = None
     resumes: list[ResumeEntity]
+
+    def __str__(self):
+        mid_name = f"Отчество: {self.mid_name}\n" if self.mid_name else ""
+        return (
+            f"Имя: {self.name}\n"
+            f"{mid_name}"
+            f"Фамилия: {self.last_name}\n"
+            f"Резюме: {'\n\n'.join((str(resume)for resume in self.resumes))}"
+        )
