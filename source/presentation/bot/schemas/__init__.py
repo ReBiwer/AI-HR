e86625e098a50fb5a404ac0360681bfcf0d89ff2
vladimir <vovka1998@gmail.com>
2025-10-24 20:59:13 +0300

    deleted schema

--- source/presentation/bot/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Сехмы для бота"""
-
-from source.presentation.bot.schemas.user import TGUser
-
-__all__ = ["TGUser"]
--- source/presentation/bot/schemas/user.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class TGUser(BaseModel):
-    hh_id: str
-    name: str
-    mid_name: str
