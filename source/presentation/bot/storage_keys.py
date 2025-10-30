d8877c1e6522ee655314b3798bcbbdb1992eb82e
vladimir <vovka1998@gmail.com>
2025-10-30 12:30:23 +0300

    added CallbacksKeys

--- source/presentation/bot/storage_keys.py
+++ source/presentation/bot/storage_keys.py
@@ -3,3 +3,11 @@ class StorageKeys:
 
     # Информация о пользователе HH
     USER_INFO = "user_data"
+
+
+class CallbackKeys:
+    """Ключи для хранения значений для вызова callback команд"""
+
+    LOGIN = "login"
+    PROFILE = "profile"
+    LOGOUT = "logout"
