700b64f71178be139577789a576ec2276a2c3019
vladimir <vovka1998@gmail.com>
2025-11-05 21:46:37 +0300

    added new keys storage and callbacks

--- source/constants/keys.py
+++ source/constants/keys.py
@@ -5,6 +5,9 @@ class StorageKeys:
     USER_INFO = "user_data"
     ACTIVE_RESUME_ID = "active_resume_id"
     ACTIVE_RESUME_TITLE = "active_resume_title"
+    AI_RESPONSE = "ai_response"
+    CURRENT_VACANCY_URL = "current_vacancy_url"
+    CURRENT_VACANCY_HH_ID = "current_vacancy_hh_id"
 
 
 class CallbackKeys:
@@ -13,3 +16,5 @@ class CallbackKeys:
     LOGIN = "login"
     PROFILE = "profile"
     LOGOUT = "logout"
+    REGENERATE_AI_RESPONSE = "regenerate_response"
+    SEND_AI_RESPONSE = "send"
